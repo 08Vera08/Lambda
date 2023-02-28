f52c752835c0c4347d6af83f50c5958488712a56
Vera Sorokina <vera.sorokina.2013@mail.ru>
2023-02-28 16:34:21 +0400

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,9 @@ import com.company.Person;
 import java.util.Collections;
 import java.util.LinkedList;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-
+        PersonComparator comp = new PersonComparator();
         LinkedList<Person> list = new LinkedList<Person>();
         list.add(new Person("James", "Van Dam", 5));
         list.add(new Person("Hretta", "Tunberg", 20));
@@ -17,7 +16,7 @@ public class Main
         list.add(new Person("John", "Murtazaliev", 7));
         list.add(new Person("Jesus", "Pavlov", 18));
         list.add(new Person("Rotor", "Pavlov", 4));
-        
+        Collections.sort(list, comp);
         list.removeIf(x -> x.getAge() < 18);
         System.out.println(list);
     }
